dfe7070b957affbf7cac9cc90674d9829cbe2810
LolichanxL <bobnev.pavel01@gmail.com>
2022-12-26 20:39:25 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+﻿#include <Game.hpp>
+
+int main()
+{
+
+    mt::Game game;
+
+    game.SetResolution(500, 600);
+    game.SetCaption("Test game");
+
+    mt::Errors error = game.Setup();
+
+    switch (error)
+    {
+    case mt::Errors::SUCCESS:
+        std::cout << "Setup complited" << std::endl;
+        break;
+    case mt::Errors::LOAD_PICTURE:
+        std::cout << "Load picture failed!" << std::endl;
+        return -1;
+    default:
+        break;
+
+    }
+
+    error = game.Run();
+
+    switch (error)
+    {
+    case mt::Errors::LOAD_PICTURE:
+        std::cout << "Load picture failed!" << std::endl;
+
+    }
+
+    return 0;
+}
---
+++ src/Game.cpp
@@ -0,0 +1,11 @@
+#include <Game.hpp>
+#include <iomanip>
+#include <thread>
+#include <chrono>
+
+namespace mt
+{
+   
+   
+}
+
