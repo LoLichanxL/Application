e57b8e7fae36abb08c262551efa5e72fcec6f05b
LolichanxL <bobnev.pavel01@gmail.com>
2022-12-26 20:57:23 +0200

    initial commit

--- inc/Game.hpp
+++ inc/Game.hpp
@@ -6,41 +6,4 @@
 
 namespace mt
 {
-    enum class Errors
-    {
-        SUCCESS = 0,
-        LOAD_PICTURE,
-    };
-
-    class Game
-    {
-    private:
-        int m_width, m_height;
-        std::string m_caption;
-        sf::RenderWindow* m_window = nullptr;
-        sf::Clock m_timer;
-        std::vector<mt::Ball*> m_balls;
-
-    private:
-        void PrepareForDisplay()
-        {
-            for (int i = 0; i < m_balls.size(); i++)
-                m_window->draw(*m_balls[i]->Get());
-        }
-
-    public:
-        Game()
-        {
-        }
-
-        ~Game();
-
-        void SetCaption(const std::string& caption);
-
-        void SetResolution(int width, int height);
-
-        mt::Errors Setup();
-
-        mt::Errors Run();
-    };
-}
+ }
--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -2,17 +2,4 @@
 
 namespace mt::utils
 {
-	struct Point
-	{
-		float x;
-		float y;
-	};
-
-	struct Vec
-	{
-		float x;
-		float y;
-	};
-
-	float Distance(Point p1, Point p2);
 }
